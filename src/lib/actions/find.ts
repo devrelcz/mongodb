17a8d75f04d03e2837330362c3d80acc1c707289
Daniel Rataj <daniel.rataj@mall.cz>
2024-04-04 10:27:09 +0200

    update json handling

--- src/lib/actions/find.ts
+++ src/lib/actions/find.ts
@@ -21,7 +21,7 @@ export const find = createAction({
     filter: Property.Json({
       displayName: 'Filter',
       description: '',
-      required: true
+      required: false
     }),
     projection: Property.Json({
       displayName: 'Projection',
@@ -30,7 +30,7 @@ export const find = createAction({
     }),
     limit: Property.Number({
       displayName: 'Limit',
-      description: 'No matter what the limit is, the maximum number of returned results in the OUTPUT is limited to 10.',
+      description: 'No matter what the limit is, the maximum number of returned results in the OUTPUT in Activepieces is limited to 10.',
       required: false,
       defaultValue: 0
     }),
@@ -60,8 +60,43 @@ export const find = createAction({
         opts.limit = context.propsValue.limit || 0;
       }
 
+      // filter example
+      // {
+      //   "$or": [
+      //     {
+      //       "type": "estates-to-buy--new"
+      //     }
+      //   ],
+      //   "createdAt": {
+      //     "$gte": "ISODate('2024-04-03T00:00:00.000Z')"
+      //   }
+      // }
+      let filter = context.propsValue.filter as object
+      let updatedFilter
+
+      if (filter) {
+        // deep recursive function to convert string ISODate with the content to Date object, key is the same
+        const convertISODate = (obj: { [x: string]: string | object }) => {
+          for (const key in obj) {
+            if (typeof obj[key] === 'object') {
+              convertISODate(obj[key] as { [x: string]: string | object });
+            } else {
+              if (typeof obj[key] === 'string' && (obj[key] as string).includes('ISODate')) {
+                obj[key] = new Date((obj[key] as string).replace(/ISODate\(|\)|'/g, ''));
+              }
+            }
+          }
+        };
+
+        updatedFilter = convertISODate(filter as { [x: string]: string | object });
+      }
+
+      if (updatedFilter !== undefined) {
+        filter = updatedFilter;
+      }
+
       const coll = client.db(context.propsValue.db).collection(context.propsValue.collection);
-      const cursor = coll.find(context.propsValue.filter, opts)
+      const cursor = coll.find(filter, opts)
       const results = await cursor.toArray();
 
       // return first ten records from results array to save page load
@@ -69,7 +104,7 @@ export const find = createAction({
         return results.slice(0, 10);
       }
 
-      return results
+      return results || []
     } catch (error) {
       return error;
     } finally {
